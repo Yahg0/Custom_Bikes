571b48f5af157c1305d7cbc1fc33618ef6fb46e5
Yahgo Batista <Yahgobatista@LIVE.COM>
2018-06-17 14:07:19 -0300

    Ligando as classes (Aro, Banco, Categoria, Guidao, Pneu e Quadro) a classe Bicicleta

--- CustomBikes/CustomBikes/Models/Bicicleta.cs
+++ CustomBikes/CustomBikes/Models/Bicicleta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,15 @@ namespace CustomBikes.Models
 {
     public class Bicicleta
     {
+        public int BicicletaID { get; set; }
+        [Required, StringLength(30)]
+        public string Nome { get; set; }
+
+        public virtual Aro _Aro { get; set; }
+        public virtual Banco _Banco { get; set; }
+        public virtual Categoria _Categoria { get; set; }
+        public virtual Guidao _Guidao { get; set; }
+        public virtual Pneu _Pneu { get; set; }
+        public virtual Quadro _Quadro { get; set; }
     }
 }
